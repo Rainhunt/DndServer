6277be885eeb9feaa44f2af915c79afef88e14cd
Lesigh-3100 <kevin@bancor.network>
2025-03-29 10:55:11 +0300

    Remove dist folder from version control

--- dist/db/schemas/PasswordField.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const passwordField = {
-    type: String,
-    required: true,
-    match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/,
-};
-exports.default = passwordField;
